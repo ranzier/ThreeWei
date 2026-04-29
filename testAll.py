faf5b4f642581529c8a91b520a8b700b5b0f7b1a
ranzier <2964464182@qq.com>
2026-04-29 21:56:56 +0800

    4/29更新-正常1

--- testAll.py
+++ testAll.py
@@ -1,9 +1,13 @@
 import tran2dto3d
 
-# danjia_dir = r"D:\Sanwei\zuobiao\DanJia\1E2-SDJ"
-# tashen_dir = r"D:\Sanwei\zuobiao\TaShen\1E2-SDJ"
-danjia_dir = r"D:\Sanwei\zuobiao\DanJia\J1"
-tashen_dir = r"D:\Sanwei\zuobiao\TaShen\J1"
+danjia_dir = r"D:\Sanwei\zuobiao\DanJia\1E2-SDJ"
+tashen_dir = r"D:\Sanwei\zuobiao\TaShen\1E2-SDJ"
+# danjia_dir = r"D:\Sanwei\zuobiao\DanJia\J1"
+# tashen_dir = r"D:\Sanwei\zuobiao\TaShen\J1"
+# danjia_dir = r"D:\Sanwei\zuobiao\DanJia\J3"
+# tashen_dir = r"D:\Sanwei\zuobiao\TaShen\J3"
+# danjia_dir = r"D:\Sanwei\zuobiao\DanJia\J4"
+# tashen_dir = r"D:\Sanwei\zuobiao\TaShen\J4"
 project_path = r"D:\Sanwei\output_path"
 savepath_ui = r"D:\Sanwei\output_path\savepath_ui"
 
@@ -11,7 +15,7 @@ tran2dto3d.tran2dto3d( danjia_dir=danjia_dir,
         tashen_dir=tashen_dir,
         project_path=project_path,
         savepath_ui=savepath_ui,
-        drawing_type ="J1")
+        drawing_type ="1E2-SDJ")
 
 
 # 1E2-SDJ依次输入：0  911  0.9
